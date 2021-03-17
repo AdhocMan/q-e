f4640a1a112c9c3e6862c3ffd441ad953b9da5d7
Simon Frasch <simon.frasch@cscs.ch>
2021-03-17 11:56:47 +0100

    use c_loc for spla interface

--- LAXlib/la_compute_distmat.f90
+++ LAXlib/la_compute_distmat.f90
@@ -57,8 +57,8 @@ SUBROUTINE laxlib_compute_distmat_z( dm, kdim, alpha, v, ldv, w, ldw, idesc, &
    CALL errore( ' laxlib_compute_distmat ',' error when calling SPLA ', ABS(status_spla) )
  !
  status_spla = spla_pzgemm_ssbtr(idesc(LAX_DESC_N), idesc(LAX_DESC_N) - nb1 + 1, kdim, &
-          SPLA_OP_CONJ_TRANSPOSE, alpha, v, ldv, w(:, nb1), ldw, ONE, dm, nx, 0, &
-          nb1 - 1, SPLA_FILL_MODE_UPPER, mat_dis_spla, ctx_spla)
+          SPLA_OP_CONJ_TRANSPOSE, alpha, c_loc(v(1,1)), ldv, c_loc(w(1, nb1)), ldw, ONE, &
+          c_loc(dm(1,1)), nx, 0, nb1 - 1, SPLA_FILL_MODE_UPPER, mat_dis_spla, ctx_spla)
  IF( status_spla /= SPLA_SUCCESS ) &
    CALL errore( ' laxlib_compute_distmat ',' error when calling SPLA ', ABS(status_spla) )
  !
